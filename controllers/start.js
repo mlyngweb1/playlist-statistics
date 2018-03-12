444acc839f4adb0b3fb2322e1faab70dcd004ad3
Glitch (ml-playlist3-add) <none>
2018-03-12 15:19:17 +0000

    👓🏩 Checkpoint controllers/start.js:633981/80 controllers/dashboard.js:633981/2 models/playlist-store.js:633981/2 views/start.hbs:633981/109 views/partials/listplaylists.hbs:633981/1

--- controllers/start.js
+++ controllers/start.js
@@ -1,12 +1,22 @@
 'use strict';
 
 const logger = require('../utils/logger');
+const playlistStore = require('../models/playlist-store');
 
 const start = {
   index(request, response) {
     logger.info('start rendering');
+    
+    const playCollections = playlistStore.getAllPlaylists();
+    let totalsongs = 0;
+    for (let i = 0; i < playCollections.length; i++) {
+     totalsongs = totalsongs + playCollections[i].songs.length;
+    }
+
     const viewData = {
       title: 'Welcome to Playlist 1',
+      totalCollections: playCollections.length,
+      totalsongs: totalsongs,
     };
     response.render('start', viewData);
   },
